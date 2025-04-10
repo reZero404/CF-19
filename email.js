df4581e55849c0d714ca4c38bbd340b4689f7118
Avik-0005 <avikpramanick05@gmail.com>
2025-04-10 10:25:31 +0530

    Modified pages

--- email.js
+++ email.js
@@ -1,9 +1,9 @@
 function sendmail(){
-    let params ={
+    let parms ={
         name : document.getElementById("name").value,
         email : document.getElementById("email").value,
         subject : document.getElementById("subject").value,
         message : document.getElementById("message").value,
     }
-    emailjs.send("service_wa3ozpk","template_2eg86jm",params).then(alert("Email Sent!!"))
+    emailjs.send("service_wa3ozpk","template_2eg86jm",parms).then(alert("Email Sent!! "))
 }
